86ad13d403305aadef0b4888ad252a9be90c3be6
Domowe <marcin_nowak_hrx@o2.pl>
2024-07-03 14:27:06 +0200

    initial

---
+++ names.js
@@ -0,0 +1,22 @@
+export const sign = {
+  type: "picker",
+  list: [
+    "Gwiazda - Odważna/brawurowa",
+    "Koło - Robotna/bez wyobraźni",
+    "Żołądź - Dociekliwa/uparta",
+    "Burza - Szczodra/gniewna",
+    "Księżyc - Mądra/nieprzenikniona",
+    "Matka - Opiekuńcza/zamartwiająca się",
+  ],
+};
+
+
+
+
+
+
+// export const something = {
+//   type: "picker",
+//   list: [
+//   ],
+// };
